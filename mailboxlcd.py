817d43da1afdf01cdbed284f2407d56477d24f29
Oskar Elvkull <oskar.elvkull@gmail.com>
2021-07-23 22:03:38 +0200

    added pi functions

--- mailboxlcd.py
+++ mailboxlcd.py
@@ -5,18 +5,17 @@ from time import sleep
 
 
 def display_message(message, times=5):
-    print(message)
-    # lcd = LCD()
-    # msg = message['message']
-    # signedby = message['signedby']
+    lcd = LCD()
+    msg = message['message']
+    signedby = message['signedby']
 
-    # msg = 16*" " + msg + 16*" "
+    msg = 16*" " + msg + 16*" "
 
-    # lcd.text(f"- {signedby}", 2)
+    lcd.text(f"- {signedby}", 2)
     
-    # for _ in range(times):
-    #     for x in range(len(msg) + 16 ):
-    #         display_text = msg[x: x+16]
-    #         lcd.text(display_text, 1)
-    #         sleep(0.5)
-    # lcd.clear()
+    for _ in range(times):
+        for x in range(len(msg) + 16 ):
+            display_text = msg[x: x+16]
+            lcd.text(display_text, 1)
+            sleep(0.5)
+    lcd.clear()
